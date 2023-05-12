99dcc0f2af5c7b715ee40ab07e3973ce3aceba02
wsz <305185235@qq.com>
2023-05-12 18:26:17 +0800

    Modify password verification and processing

--- src/layout/Header/components/PersonalDialog.vue
+++ src/layout/Header/components/PersonalDialog.vue
@@ -11,10 +11,10 @@
       <el-form-item label="姓名">
         <el-input v-model="ruleForm.name" disabled></el-input>
       </el-form-item>
-      <el-form-item label="新密码" prop="password">
+      <el-form-item label="新密码" prop="newPassword">
         <el-input v-model="ruleForm.newPassword" type="password"></el-input>
       </el-form-item>
-      <el-form-item label="确认密码" prop="configPassword">
+      <el-form-item label="确认密码" prop="confirmPassword">
         <el-input v-model="ruleForm.confirmPassword" type="password"></el-input>
       </el-form-item>
     </el-form>
@@ -56,55 +56,59 @@
     confirmPassword: '',
   })
 
-  const methods = {
-    validateConfirmPassword: (_, value) => {
-      if (value === ruleForm.newPassword) {
-        console.log(ruleForm.newPassword)
-        console.log(value)
-        return Promise.resolve();
-      } else {
-        console.log(ruleForm.newPassword)
-        console.log(value)
-        return Promise.reject(new Error('两次密码输入不一致'));
-      }
+  const validateNewPassword=(rule: any, value: any, callback: any) => {
+    if (!/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,16}$/.test(value)) {
+      return Promise.reject(new Error('6到16位的数字字母组合'));
+    }
+    return Promise.resolve();
+  }
+
+  const validateConfirmPassword=(rule: any, value: any, callback: any) => {
+    if (!/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,16}$/.test(value)) {
+      return Promise.reject(new Error('6到16位的数字字母组合'));
+    }
+    if (value === ruleForm.newPassword) {
+      return Promise.resolve();
+    } else {
+      return Promise.reject(new Error('两次密码输入不一致'));
     }
   }
 
   const rules = reactive({
     newPassword: [
-      {
-        required: true,
-        message: '请输入新的密码',
-        trigger: 'blur',
-      }
+      {required: true, message: '请输入新的密码', trigger: 'blur'},
+      {validator: validateNewPassword, trigger: 'blur'}
     ],
     confirmPassword: [
       {required: true, message: '请再次输入新的密码', trigger: 'blur'},
-      {validator: methods.validateConfirmPassword, trigger: 'blur'}
+      {validator: validateConfirmPassword, trigger: 'blur'}
     ],
-  })
+  }
 
 
   const submitForm = (formEl: FormInstance | undefined) => {
     if (!formEl) return
     formEl.validate(async (valid) => {
       if (valid) {
-        if (ruleForm.newPassword===ruleForm.confirmPassword){
-          let email=UserStore.userInfo.email
-          let token=UserStore.userInfo.token
-          let resetMsg=await resetPassword(email,ruleForm.confirmPassword,token)
-          if (resetMsg.code==0) {
-            hide()
-            ElNotification({
-              title: getTimeState(),
-              message: '更改密码成功',
-              type: 'success',
-              duration: 2000,
-            });
-          }
-          console.log('submit!')
+        let email=UserStore.userInfo.email
+        let token=UserStore.userInfo.token
+        let resetMsg=await resetPassword(email,ruleForm.confirmPassword,token)
+        if (resetMsg.code==0) {
+          hide()
+          ElNotification({
+            title: getTimeState(),
+            message: '更改密码成功',
+            type: 'success',
+            duration: 2000,
+          });
         }else {
-          console.log('error!')
+          hide()
+          ElNotification({
+            title: getTimeState(),
+            message: '更改密码失败',
+            type: 'error',
+            duration: 2000,
+          });
         }
       } else {
         console.log('error submit!')
